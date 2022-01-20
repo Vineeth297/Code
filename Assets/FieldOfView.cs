dcf4430c0c12e269fd549674aa50f2cbf8808e73
VineethKumar <mamidipallyvineeth@gmail.com>
2022-01-20 11:28:17 +0530

    Update
    Will be using FieldOfView and its collider to detect player
    
    COntinue to work on attcking the player

--- Assets/FieldOfView.cs
+++ Assets/FieldOfView.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
-    
+	private Transform _parent;
+	private void Start()
+	{
+		_parent = transform.root;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			print("Found Player");
+		}
+	}
+
+	private IEnumerator ChasePlayer()
+	{
+		//Locate Player
+		
+		//Attack Player
+		//Chase Player
+		yield return null;
+	}
 }
